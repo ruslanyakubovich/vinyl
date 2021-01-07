554b906307e58daef07f7ab5e323d56aa55cd181
yaruslan <ruslan@yaruslan.ru>
2021-01-07 21:59:14 +0300

    получение данных коллекции

--- ini.sh
+++ ini.sh
@@ -8,48 +8,72 @@ define('dAuthorization', sprintf('Authorization: Discogs key=%s, secret=%s', dLo
 $Title = [
 	'ID' =>  7,
 	'artist' => 1,
-	'attle' =>  2,
+	'tittle' =>  2,
 	'released' =>  6,
 	'folder' =>  8,
-	'notes' =>  11,
-	'json' =>  12,
+	'notes' =>  11
+	//'json' =>  12,
 ];
 
 
 // исключить эти папки
 $folders = ['archive', 'Uncategorized'];
 
+// оставить эти значения
+$SetKeys = ["uri", "genres", "styles", "images", "thumb"];
+
+function ClearKeys($keys, $arr){
+	foreach ($arr as $k => $v) 
+		if (!in_array($k, $keys))
+			unset($arr[$k]);
+	return $arr;
+}
+
 $csvFile = file(fileCSV);
 unset($csvFile[0]);
 $data = [];
 $d = [];
 foreach ($csvFile as $line) {
+	$line = str_replace('₽', '', $line);
 	$d = str_getcsv($line);
-	$v = [];
 	if (in_array($d[$Title['folder']], $folders))
 		continue;
+
+	$v = ['num' => count($data)];
 	foreach ($Title as $key => $value) 
-		$v[$key] = $d[$value];
+		$v[$key] = $d[$value]; 
 	$data[] = $v;
+
+	// if (count($data) == 5)
+	// 	break;
 }
 
 
-$curlSession = curl_init();
 
-curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
-curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curlSession, CURLOPT_USERAGENT, 'FooBarApp/3.0');
-curl_setopt($curlSession, CURLOPT_HTTPHEADER, [dAuthorization]);
+$count = count($data);
+foreach ($data as $i => $v) { 
+	$curlSession = curl_init();
+	curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
+	curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curlSession, CURLOPT_USERAGENT, 'FooBarApp/3.0');
+	curl_setopt($curlSession, CURLOPT_HTTPHEADER, [dAuthorization]);
 
 
-for ($i=0; $i < 2; $i++) { 
-	$u = sprintf(dAPI, $data[$i]['ID']);
-	printf("%d > %s\n",$i,  $u);
+	$u = sprintf(dAPI, $v['ID']);
+	printf("%d/%d > %s\n", $i, $count, $u);
 	curl_setopt($curlSession, CURLOPT_URL, $u);
 	$j = json_decode(curl_exec($curlSession), true);
-	$data[$i]['json'] = $j;	
-}
+	$j = ClearKeys($SetKeys, $j);
+	foreach ($j as $jk => $jv) 
+		$data[$i][$jk] = $jv;
+	
+	if (rand(0,5) == rand(0,3)){
+		file_put_contents('collection.json', json_encode($data));
+		echo "sleep 2\n";
+		sleep(2);
+	}
 
-curl_close($curlSession);
+	curl_close($curlSession);
+}
 
 file_put_contents('collection.json', json_encode($data));
